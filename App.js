bf031947d26206328337d6de467cc0708c51b15e
René Wilby <rbrki07@gmail.com>
2023-09-02 11:31:54 +0200

    inital commit

--- App.js
+++ App.js
@@ -1,11 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// @ts-check
+import React, { useEffect, useState } from "react"
+import { Button, Platform, StyleSheet, Text, View } from 'react-native';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import Constants from "expo-constants";
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState(undefined);
+
+  const [notificationsPermissions, setNotificationsPermissions] = useState({
+    canAskAgain: undefined,
+    expires: undefined,
+    granted: undefined,
+    status: undefined,
+  });
+
+  useEffect(() => {
+    const getNotificationsPermissions = async () => {
+      const { canAskAgain, expires, granted, status } = await Notifications.getPermissionsAsync();
+      // @ts-ignore
+      setNotificationsPermissions({ canAskAgain, expires, granted, status });
+    };
+    getNotificationsPermissions();
+  }, [])
+
+  console.log('notificationsPermissions', notificationsPermissions)
+
+  useEffect(() => {
+    if (notificationsPermissions.granted === true) {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+          shouldPlaySound: false,
+          shouldSetBadge: false,
+        }),
+      });
+    }
+  }, [notificationsPermissions.granted])
+
+  useEffect(() => {
+    if (notificationsPermissions.granted === true && Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync('default', {
+        name: 'default',
+        importance: Notifications.AndroidImportance.DEFAULT
+      })
+    }
+  }, [notificationsPermissions.granted])
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(notification => {
+      console.log('Received notification while app is running in foreground', notification)
+    });
+    return () => {
+      subscription.remove();
+    }
+  }, [])
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log('User interacted with notification', response.notification)
+    });
+    return () => {
+      subscription.remove();
+    }
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {notificationsPermissions.granted === false && notificationsPermissions.canAskAgain === true &&
+        <Button 
+          title={'Request notification permission'} 
+          onPress={async () => {
+            const { canAskAgain, expires, granted, status } =
+              await Notifications.requestPermissionsAsync({
+                ios: {
+                  allowAlert: true,
+                  allowBadge: true,
+                  allowSound: true,
+                },
+              });
+            // @ts-ignore
+            setNotificationsPermissions({ canAskAgain, expires, granted, status });
+          }}
+        />
+      }
+      {Device.isDevice === false &&
+        <Text>Push notifications don't work on emulators/simulators.</Text>
+      }
+      {notificationsPermissions.granted === true && Device.isDevice && expoPushToken === undefined && 
+        <Button 
+          title={'Get Expo Push-Token'}
+          onPress={async () => {
+            const expoPushToken = await Notifications.getExpoPushTokenAsync({
+              projectId: Constants.expoConfig?.extra?.eas.projectId,
+            });
+            console.log('expoPushToken', expoPushToken);
+            // @ts-ignore
+            setExpoPushToken(expoPushToken);
+          }}
+        />
+      }
+      {expoPushToken && 
+        <View>
+          <Text>Expo Push-Token:</Text>
+          {/* @ts-ignore */}
+          <Text>{expoPushToken?.data}</Text>
+        </View>
+      }
     </View>
   );
 }
---
+++ README.md
@@ -0,0 +1,15 @@
+### Push-Notification Example App
+
+Demo-App für Schulungszwecke.
+
+Die App basiert auf React Native und Expo.
+
+## Installationsanleitung
+
+1. Repository clonen
+2. In das Projektverzeichnis wechseln
+3. `npm install` ausführen
+4. `sudo npm install -g eas-cli` ausführen
+5. `eas login` ausführen
+6. `eas init` ausführen
+7. `npm start` ausführen
